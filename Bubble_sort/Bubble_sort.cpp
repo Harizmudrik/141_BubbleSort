a113291d54c0a049290cbf9e54a029d619616dfa
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-03-16 21:07:15 +0700

    Tambahan void Unsorted

--- Bubble_sort/Bubble_sort.cpp
+++ Bubble_sort/Bubble_sort.cpp
@@ -39,7 +39,16 @@ void bubbleSortArray() {
     }
 }
 
-
+void unsorted() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang belum tersusun" << endl;
+    cout << "=================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << a[j] << endl;
+    }
+    cout << endl;
+}
 
 int main()
 {
